1a2fcf04c3dd28dd01fe18d46146ed8c0c15863b
Brandon Presley <brandon@presley.nz>
2022-07-11 13:06:16 +1200

    Fix drawer title when going back on Best

--- BestPage.tsx
+++ BestPage.tsx
@@ -32,7 +32,7 @@ export default function BestPage() {
               headerLeft: () => (
                 <IconButton icon="menu" onPress={navigation.openDrawer} />
               ),
-              title: 'Home',
+              title: 'Best',
             });
           },
         }}
