477f28775a6574bde1cf33e90d2e0f7039eb6be3
Brandon Presley <brandon@presley.nz>
2022-07-26 12:35:51 +1200

    Add script to release a new version

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -ex
+cd android || exit 1
+target=app/build.gradle 
+newVersion=$(grep '^\s*versionCode [0-9]*$' "$target" | awk '{print $2+1}')
+sed -i "s/\(^\s*\)versionCode [0-9]*$/\1versionCode $newVersion/" "$target"
+./gradlew bundleRelease 
+git add app/build.gradle 
+git commit -m "Bundle version $newVersion"
+cd ..
