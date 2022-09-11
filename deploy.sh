e43375ed78fd722ee95df365aa3113017247a7e8
Brandon Presley <brandon@presley.nz>
2022-09-11 15:48:11 +1200

    Remove typescript and lint from deploy
    I've put it in my git pre-commit hook instead.
    https://git-scm.com/book/en/v2/Customizing-Git-Git-Hooks

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 set -ex
-yarn tsc && yarn lint
 git push origin HEAD > /dev/null &
 cd android || exit 1
 target=app/build.gradle 
@@ -10,7 +9,7 @@ sed -i "s/\(^\s*\)versionCode [0-9]*$/\1versionCode $newVersion/" \
   "$target"
 [ "$1" != "--nobundle" ] && ./gradlew bundleRelease 
 git add app/build.gradle 
-git commit -m "Set versionCode=$newVersion"
+git commit --no-verify --message "Set versionCode=$newVersion"
 git tag "$newVersion"
 git push origin HEAD & git push --tags
 cd ..
