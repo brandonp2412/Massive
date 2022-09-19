6ddecf60f01c5684fb809f80a7ac206623342a87
Brandon Presley <brandon@presley.nz>
2022-09-19 13:23:00 +1200

    Handle VIBRATOR_SERVICE deprecation for alarms
    Closes #28.

--- android/app/src/main/java/com/massive/AlarmService.kt
+++ android/app/src/main/java/com/massive/AlarmService.kt
@@ -1,16 +1,14 @@
 package com.massive
 
 import android.app.Service
+import android.content.Context
 import android.media.MediaPlayer.OnPreparedListener
 import android.media.MediaPlayer
-import android.os.Vibrator
 import androidx.annotation.RequiresApi
 import android.content.Intent
 import android.media.AudioAttributes
 import android.net.Uri
-import android.os.Build
-import android.os.VibrationEffect
-import android.os.IBinder
+import android.os.*
 
 class AlarmService : Service(), OnPreparedListener {
     var mediaPlayer: MediaPlayer? = null
@@ -42,7 +40,14 @@ class AlarmService : Service(), OnPreparedListener {
             }
         }
         val pattern = longArrayOf(0, 300, 1300, 300, 1300, 300)
-        vibrator = applicationContext.getSystemService(VIBRATOR_SERVICE) as Vibrator
+        vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager =
+                getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator
+        } else {
+            @Suppress("DEPRECATION")
+            getSystemService(VIBRATOR_SERVICE) as Vibrator
+        }
         val audioAttributes = AudioAttributes.Builder()
             .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
             .setUsage(AudioAttributes.USAGE_ALARM)
