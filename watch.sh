ceda1cecf4ed0bedb22e8af96321b41230dc983f
Brandon Presley <brandon@presley.nz>
2022-09-20 12:54:52 +1200

    Add TypeScript watching to watch.sh

--- watch.sh
+++ watch.sh
@@ -6,4 +6,11 @@ ls *.ts* | entr yarn lint | tee >(
       notify-send "${ln}"
     fi
   done
-)
+) &
+yarn tsc --watch --preserveWatchOutput | tee >(
+  while read ln; do 
+    if echo "${ln}" | grep -q "Found [^0][0-9]* error"; then 
+      notify-send "${ln}"
+    fi
+  done
+) &
