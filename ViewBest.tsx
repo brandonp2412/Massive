f38cc9d00c7f8e75b7403ba85fe678927d53b488
Brandon Presley <brandon@presley.nz>
2022-09-20 12:28:15 +1200

    Remove react-native-picker-select
    Closes #29

--- ViewBest.tsx
+++ ViewBest.tsx
@@ -1,3 +1,4 @@
+import {Picker} from '@react-native-picker/picker';
 import {
   RouteProp,
   useFocusEffect,
@@ -5,10 +6,9 @@ import {
   useRoute,
 } from '@react-navigation/native';
 import React, {useCallback, useEffect, useState} from 'react';
-import {View} from 'react-native';
+import {useColorScheme, View} from 'react-native';
 import {FileSystem} from 'react-native-file-access';
 import {IconButton} from 'react-native-paper';
-import RNPickerSelect from 'react-native-picker-select';
 import Share from 'react-native-share';
 import {captureScreen} from 'react-native-view-shot';
 import {getVolumes, getWeightsBy} from './best.service';
@@ -23,6 +23,7 @@ import Volume from './volume';
 
 export default function ViewBest() {
   const {params} = useRoute<RouteProp<BestPageParams, 'ViewBest'>>();
+  const dark = useColorScheme() === 'dark';
   const [weights, setWeights] = useState<Set[]>([]);
   const [volumes, setVolumes] = useState<Volume[]>([]);
   const [metric, setMetric] = useState(Metrics.Weight);
@@ -67,23 +68,23 @@ export default function ViewBest() {
 
   return (
     <View style={{padding: PADDING}}>
-      <RNPickerSelect
-        onValueChange={setMetric}
-        items={[
-          {label: Metrics.Weight, value: Metrics.Weight},
-          {label: Metrics.Volume, value: Metrics.Volume},
-        ]}
-        value={metric}
-      />
-      <RNPickerSelect
-        onValueChange={setPeriod}
-        items={[
-          {label: Periods.Weekly, value: Periods.Weekly},
-          {label: Periods.Monthly, value: Periods.Monthly},
-          {label: Periods.Yearly, value: Periods.Yearly},
-        ]}
-        value={period}
-      />
+      <Picker
+        style={{color: dark ? 'white' : 'black'}}
+        dropdownIconColor={dark ? 'white' : 'black'}
+        selectedValue={metric}
+        onValueChange={value => setMetric(value)}>
+        <Picker.Item value={Metrics.Volume} label={Metrics.Volume} />
+        <Picker.Item value={Metrics.Weight} label={Metrics.Weight} />
+      </Picker>
+      <Picker
+        style={{color: dark ? 'white' : 'black'}}
+        dropdownIconColor={dark ? 'white' : 'black'}
+        selectedValue={period}
+        onValueChange={value => setPeriod(value)}>
+        <Picker.Item value={Periods.Weekly} label={Periods.Weekly} />
+        <Picker.Item value={Periods.Monthly} label={Periods.Monthly} />
+        <Picker.Item value={Periods.Yearly} label={Periods.Yearly} />
+      </Picker>
       {metric === Metrics.Volume && (
         <Chart
           yData={volumes.map(v => v.value)}
