0ebe0844650ea974a9e54b0749233b06e097ecd2
Brandon Presley <brandon@presley.nz>
2022-09-24 14:07:03 +1200

    Remove color setting from MassiveFab
    This makes the button automatically adjust
    according to what the user has set the primary
    color to be.
    Seems that with the default dark theme the cyan
    makes the text not the same color as the Button,
    but this still looks better than the statically
    set colors from before.

--- MassiveFab.tsx
+++ MassiveFab.tsx
@@ -1,19 +1,16 @@
 import React, {useContext} from 'react';
-import {useColorScheme} from 'react-native';
 import {FAB} from 'react-native-paper';
-import {CombinedDarkTheme, CustomTheme} from './App';
+import {CustomTheme} from './App';
 
 export default function MassiveFab(
   props: Partial<React.ComponentProps<typeof FAB>>,
 ) {
-  const dark = useColorScheme() === 'dark';
   const {color} = useContext(CustomTheme);
 
   return (
     <FAB
       {...props}
       icon="add"
-      color={dark ? CombinedDarkTheme.colors.background : 'white'}
       style={{
         position: 'absolute',
         right: 10,
