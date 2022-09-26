bc6eb9b713f17a927484bda1a2530e8cb082cf6f
Brandon Presley <brandon@presley.nz>
2022-09-27 12:26:11 +1300

    Swap white color for linen and add Salmon+Red
    White is impossible to see on light theme.
    I want the lighter colors to be possible to select on a light theme
    (just looks bad) up to the user what they want, but white on white
    can't actually be seen at all.

--- colors.ts
+++ colors.ts
@@ -1,11 +1,13 @@
 export const lightColors = [
   {hex: '#B3E5FC', name: 'Cyan'},
   {hex: '#FFC0CB', name: 'Pink'},
-  {hex: '#FFFFFF', name: 'White'},
+  {hex: '#E9DCC9', name: 'Linen'},
 ];
 
 export const darkColors = [
+  {hex: '#FA8072', name: 'Salmon'},
   {hex: '#8156A7', name: 'Purple'},
   {hex: '#007AFF', name: 'Blue'},
   {hex: '#000000', name: 'Black'},
+  {hex: '#CD5C5C', name: 'Red'},
 ];
