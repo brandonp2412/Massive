112c3ce71b45bf773d22d9d87dcda91011ae6e93
Brandon Presley <brandon@presley.nz>
2022-09-27 18:14:33 +1300

    Sync package.json version with android versionName
    Related to #62

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,26 @@
 set -ex
 git push origin HEAD > /dev/null &
 cd android || exit 1
-target=app/build.gradle 
-newVersion=$(grep '^\s*versionCode [0-9]*$' "$target" | awk '{print $2+1}')
-sed -i "s/\(^\s*\)versionCode [0-9]*$/\1versionCode $newVersion/" \
-  "$target"
+build=app/build.gradle 
+versionCode=$(
+  grep '^\s*versionCode [0-9]*$' "$build" | awk '{print $2+1}'
+)
+major=$(
+  grep '^\s*versionName "[0-9]*\.[0-9]*"' "$build" \
+    | sed 's/"//g' | cut -d '.' -f 1 | awk '{print $2}'
+)
+minor=$(
+  grep '^\s*versionName "[0-9]*\.[0-9]*"' "$build" \
+    | sed 's/"//g' | cut -d '.' -f 2
+)
+sed -i "s/\(^\s*\)versionCode [0-9]*$/\1versionCode $versionCode/" \
+  "$build"
+sed -i "s/\(^\s*\)versionName \"[0-9]*.[0-9]*\"$/\1versionName \"$major.$((minor+1))\"/" "$build"
+sed -i "s/\"version\": \"[0-9]*.[0-9]*\"/\"version\": \"$major.$((minor+1))\"/" ../package.json
+cd android || exit 1
 [ "$1" != "--nobundle" ] && ./gradlew bundleRelease 
 git add app/build.gradle 
-git commit --no-verify --message "Set versionCode=$newVersion"
+git commit --no-verify --message "Set versionCode=$versionCode"
 git tag "$newVersion"
 git push origin HEAD & git push --tags
 cd ..
