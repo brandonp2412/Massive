cdb4ceec28b11f90a3b4579964f13a7385cd089e
Brandon Presley <brandon@presley.nz>
2022-09-28 14:06:44 +1300

    Move salmon to light colors

--- colors.ts
+++ colors.ts
@@ -1,11 +1,11 @@
 export const lightColors = [
+  {hex: '#FA8072', name: 'Salmon'},
   {hex: '#B3E5FC', name: 'Cyan'},
   {hex: '#FFC0CB', name: 'Pink'},
   {hex: '#E9DCC9', name: 'Linen'},
 ];
 
 export const darkColors = [
-  {hex: '#FA8072', name: 'Salmon'},
   {hex: '#8156A7', name: 'Purple'},
   {hex: '#007AFF', name: 'Blue'},
   {hex: '#000000', name: 'Black'},
