83852b32162a8378f220afbef2623388b7dc2368
Brandon Presley <brandon@presley.nz>
2022-11-01 19:59:33 +1300

    Apply eslint rules to js files

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ['*.ts', '*.tsx', '*.js'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
