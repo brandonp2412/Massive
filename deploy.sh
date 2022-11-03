7760c946263653831b6a481256ff352071840c24
Brandon Presley <brandon@presley.nz>
2022-11-03 23:39:05 +1300

    Organize deploy.sh a bit

--- deploy.sh
+++ deploy.sh
@@ -7,27 +7,33 @@ yarn lint
 git push origin HEAD
 
 cd android || exit 1
-build=app/build.gradle 
+
+build=app/build.gradle
 versionCode=$(
-  grep '^\s*versionCode [0-9]*$' "$build" | awk '{print $2+1}'
+	grep '^\s*versionCode [0-9]*$' "$build" | awk '{print $2+1}'
 )
 major=$(
-  grep '^\s*versionName "[0-9]*\.[0-9]*"' "$build" \
-    | sed 's/"//g' | cut -d '.' -f 1 | awk '{print $2}'
+	grep '^\s*versionName "[0-9]*\.[0-9]*"' "$build" |
+		sed 's/"//g' | cut -d '.' -f 1 | awk '{print $2}'
 )
 minor=$(
-  grep '^\s*versionName "[0-9]*\.[0-9]*"' "$build" \
-    | sed 's/"//g' | cut -d '.' -f 2
+	grep '^\s*versionName "[0-9]*\.[0-9]*"' "$build" |
+		sed 's/"//g' | cut -d '.' -f 2
 )
-minor=$((minor+1))
+minor=$((minor + 1))
+
 sed -i "s/\(^\s*\)versionCode [0-9]*$/\1versionCode $versionCode/" \
-  "$build"
+	"$build"
 sed -i "s/\(^\s*\)versionName \"[0-9]*.[0-9]*\"$/\1versionName \"$major.$minor\"/" "$build"
 sed -i "s/\"version\": \"[0-9]*.[0-9]*\"/\"version\": \"$major.$minor\"/" ../package.json
-[ "$1" != "--nobundle" ] && ./gradlew bundleRelease 
+
+[ "$1" != "--nobundle" ] && ./gradlew bundleRelease
+
 git add app/build.gradle ../package.json
 git commit --no-verify --message "Set versionCode=$versionCode"
 git tag "$versionCode"
-git push origin HEAD & git push --tags
+git push origin HEAD &
+git push --tags
+
 cd ..
 ./install.sh
