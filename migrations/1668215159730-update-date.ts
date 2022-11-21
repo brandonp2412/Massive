a001760ab0f34893625725a163b2e92976269adc
Brandon Presley <brandon@presley.nz>
2022-11-21 17:53:14 +1300

    Just clear users date setting in update-date migration
    This switch is probably missing some old ones,
    easier to just wipe it.

--- migrations/1668215159730-update-date.ts
+++ migrations/1668215159730-update-date.ts
@@ -4,27 +4,7 @@ import {settingsRepo} from '../db'
 export class updateDate1668215159730 implements MigrationInterface {
   public async up(_queryRunner: QueryRunner): Promise<void> {
     const settings = await settingsRepo.findOne({where: {}})
-    let newDate = 'P'
-    switch (settings.date) {
-      case '%Y-%m-%d %H:%M':
-      case '%d/%m/%y %h:%M %p':
-      case '%d/%m %h:%M %p':
-        newDate = 'Pp'
-        break
-      case '%Y-%m-%d':
-      case '%d/%m/%y':
-      case '%d/%m':
-        newDate = 'P'
-        break
-      case '%H:%M':
-      case '%h:%M %p':
-        newDate = 'p'
-        break
-      case '%A %h:%M %p':
-        newDate = 'cccc p'
-        break
-    }
-    settings.date = newDate
+    settings.date = 'P'
     await settingsRepo.save(settings)
   }
 
