e430873771d348a1b931bd5b0c038b1db827a134
Brandon Presley <brandon@presley.nz>
2022-12-30 12:35:19 +1300

    Remove commented code from jestSetup

--- jestSetup.ts
+++ jestSetup.ts
@@ -1,19 +1,6 @@
 import 'react-native-gesture-handler/jestSetup'
 import {NativeModules as RNNativeModules} from 'react-native'
 
-//RNNativeModules.UIManager = RNNativeModules.UIManager || {};
-//RNNativeModules.UIManager.RCTView = RNNativeModules.UIManager.RCTView || {};
-//RNNativeModules.RNGestureHandlerModule =
-//  RNNativeModules.RNGestureHandlerModule || {
-//    State: {BEGAN: 'BEGAN', FAILED: 'FAILED', ACTIVE: 'ACTIVE', END: 'END'},
-//    attachGestureHandler: jest.fn(),
-//    createGestureHandler: jest.fn(),
-//    dropGestureHandler: jest.fn(),
-//    updateGestureHandler: jest.fn(),
-//  };
-//RNNativeModules.PlatformConstants = RNNativeModules.PlatformConstants || {
-//  forceTouchAvailable: false,
-//};
 RNNativeModules.RNViewShot = RNNativeModules.RNViewShot || {
   captureScreen: jest.fn(),
 }
