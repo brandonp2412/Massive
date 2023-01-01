13e1d4cc2194132b1da08fd257c1a81fadc7f064
Brandon Presley <brandon@presley.nz>
2023-01-01 14:19:15 +1300

    Move the theme object to be inline for mock-providers
    This way we get better auto completion

--- mock-providers.tsx
+++ mock-providers.tsx
@@ -8,22 +8,21 @@ import {
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import {ThemeContext} from './use-theme'
 
-export const theme = {
-  theme: 'system',
-  setTheme: jest.fn(),
-  lightColor: DefaultTheme.colors.primary,
-  darkColor: DarkTheme.colors.primary,
-  setLightColor: jest.fn(),
-  setDarkColor: jest.fn(),
-}
-
 export const MockProviders = ({
   children,
 }: {
   children: JSX.Element | JSX.Element[]
 }) => (
   <PaperProvider settings={{icon: props => <MaterialIcon {...props} />}}>
-    <ThemeContext.Provider value={theme}>
+    <ThemeContext.Provider
+      value={{
+        theme: 'system',
+        setTheme: jest.fn(),
+        lightColor: DefaultTheme.colors.primary,
+        darkColor: DarkTheme.colors.primary,
+        setLightColor: jest.fn(),
+        setDarkColor: jest.fn(),
+      }}>
       <NavigationContainer>{children}</NavigationContainer>
     </ThemeContext.Provider>
   </PaperProvider>
