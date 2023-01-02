d0702b767564f7d965fcbb172b27da85760a83e5
Brandon Presley <brandon@presley.nz>
2023-01-03 11:31:25 +1300

    Reduce redundancy of labels for theme setting

--- options.ts
+++ options.ts
@@ -1,9 +1,9 @@
 import {darkColors, lightColors} from './colors'
 
 export const themeOptions = [
-  {label: 'Follow system theme', value: 'system'},
-  {label: 'Dark theme', value: 'dark'},
-  {label: 'Light theme', value: 'light'},
+  {label: 'System', value: 'system'},
+  {label: 'Dark', value: 'dark'},
+  {label: 'Light', value: 'light'},
 ]
 
 export const lightOptions = lightColors.map(color => ({
