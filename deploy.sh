9f7cbba80a9d5971a91b4ec6a2462e90fc583eee
Brandon <brandon@presley.nz>
2023-02-04 14:14:29 -0700

    Add -n flag to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -27,17 +27,15 @@ sed -i "s/\(^\s*\)versionCode [0-9]*$/\1versionCode $versionCode/" \
 sed -i "s/\(^\s*\)versionName \"[0-9]*.[0-9]*\"$/\1versionName \"$major.$minor\"/" "$build"
 sed -i "s/\"version\": \"[0-9]*.[0-9]*\"/\"version\": \"$major.$minor\"/" ../package.json
 
-[ "$1" != "--nobundle" ] && ./gradlew bundleRelease
-
-bundle install
-bundle exec fastlane supply --aab app/build/outputs/bundle/release/app-release.aab
+if [ "$1" != "-n" ]; then
+  ./gradlew bundleRelease
+  bundle install
+  bundle exec fastlane supply --aab app/build/outputs/bundle/release/app-release.aab
+fi
 
 git add app/build.gradle ../package.json
 git commit --amend --message \
   "$(git log -1 --pretty=%B | sed " 1 s/.*/& - $major.$minor/")"
 git tag "$versionCode"
-git push origin HEAD &
+git push origin HEAD
 git push --tags
-
-cd ..
-./install.sh
