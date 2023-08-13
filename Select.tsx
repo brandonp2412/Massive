79cde3a2191bb8e77849f7eee4edc18755191e17
Brandon Presley <brandon@presley.nz>
2023-08-14 10:54:46 +1200

    Use accurate theme color for switch text
    Only if no custom color is provided

--- Select.tsx
+++ Select.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useMemo, useState } from "react";
 import { View } from "react-native";
-import { Button, Menu, Subheading } from "react-native-paper";
+import { Button, Menu, Subheading, useTheme } from "react-native-paper";
 import { ITEM_PADDING } from "./constants";
 
 export interface Item {
@@ -21,6 +21,7 @@ function Select({
   label?: string;
 }) {
   const [show, setShow] = useState(false);
+  const { colors } = useTheme();
 
   const selected = useMemo(
     () => items.find((item) => item.value === value) || items[0],
@@ -60,7 +61,7 @@ function Select({
       >
         {items.map((item) => (
           <Menu.Item
-            titleStyle={{ color: item.color }}
+            titleStyle={{ color: item.color || colors.onSurface }}
             key={item.value}
             title={item.label}
             onPress={() => handlePress(item.value)}
