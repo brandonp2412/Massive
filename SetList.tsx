49646c3107991337a4b5f85a6f1d89ce40dad0ad
Brandon Presley <brandon@presley.nz>
2023-11-13 17:10:40 +1300

    Reduce default limit back to 15
    The feeling of scrolling was nice but a page size of 50
    makes the main page a bit slower which isn't worth it.

--- SetList.tsx
+++ SetList.tsx
@@ -148,7 +148,7 @@ export default function SetList() {
         style={{ flex: 1 }}
         renderItem={renderItem}
         onEndReached={next}
-        onEndReachedThreshold={0.99}
+        onEndReachedThreshold={0.5}
         refreshing={refreshing}
         keyExtractor={(set) => set.id?.toString()}
         onRefresh={() => {
