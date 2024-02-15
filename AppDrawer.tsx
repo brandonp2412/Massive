183d609beab6bc433e8b74b0d0d6830960ffa3b9
Brandon Presley <brandon@presley.nz>
2024-02-15 14:10:40 +1300

    Add indicator for being in debug mode

--- AppDrawer.tsx
+++ AppDrawer.tsx
@@ -1,6 +1,6 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { StackScreenProps } from "@react-navigation/stack";
-import { IconButton, useTheme } from "react-native-paper";
+import { IconButton, useTheme, Banner } from "react-native-paper";
 import { DrawerParams } from "./drawer-params";
 import ExerciseList from "./ExerciseList";
 import GraphsList from "./GraphsList";
@@ -10,6 +10,7 @@ import SetList from "./SetList";
 import SettingsPage from "./SettingsPage";
 import TimerPage from "./TimerPage";
 import WeightList from "./WeightList";
+import { StyleSheet, Text, View } from "react-native";
 
 const Drawer = createDrawerNavigator<DrawerParams>();
 
@@ -23,60 +24,86 @@ export default function AppDrawer({
   const { dark } = useTheme();
 
   return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerTintColor: dark ? "white" : "black",
-        swipeEdgeWidth: 1000,
-        headerShown: false,
-      }}
-      initialRouteName={
-        (route.params.startup as keyof DrawerParams) || "History"
-      }
-    >
-      <Drawer.Screen
-        name="History"
-        component={SetList}
-        options={{ drawerIcon: () => <IconButton icon="history" /> }}
-      />
-      <Drawer.Screen
-        name="Exercises"
-        component={ExerciseList}
-        options={{ drawerIcon: () => <IconButton icon="dumbbell" /> }}
-      />
-      <Drawer.Screen
-        name="Plans"
-        component={PlanList}
-        options={{ drawerIcon: () => <IconButton icon="calendar-outline" /> }}
-      />
-      <Drawer.Screen
-        name="Graphs"
-        component={GraphsList}
-        options={{
-          drawerIcon: () => <IconButton icon="chart-bell-curve-cumulative" />,
+    <>
+      {__DEV__ && (
+        <View style={styles.debugBanner}>
+          <Text style={styles.debugText}>DEBUG</Text>
+        </View>
+      )}
+      <Drawer.Navigator
+        screenOptions={{
+          headerTintColor: dark ? "white" : "black",
+          swipeEdgeWidth: 1000,
+          headerShown: false,
         }}
-      />
-      <Drawer.Screen
-        name="Timer"
-        component={TimerPage}
-        options={{ drawerIcon: () => <IconButton icon="timer-outline" /> }}
-      />
-      <Drawer.Screen
-        name="Weight"
-        component={WeightList}
-        options={{ drawerIcon: () => <IconButton icon="scale-bathroom" /> }}
-      />
-      <Drawer.Screen
-        name="Insights"
-        component={InsightsPage}
-        options={{
-          drawerIcon: () => <IconButton icon="lightbulb-on-outline" />,
-        }}
-      />
-      <Drawer.Screen
-        name="Settings"
-        component={SettingsPage}
-        options={{ drawerIcon: () => <IconButton icon="cog-outline" /> }}
-      />
-    </Drawer.Navigator>
+        initialRouteName={
+          (route.params.startup as keyof DrawerParams) || "History"
+        }
+      >
+        <Drawer.Screen
+          name="History"
+          component={SetList}
+          options={{ drawerIcon: () => <IconButton icon="history" /> }}
+        />
+        <Drawer.Screen
+          name="Exercises"
+          component={ExerciseList}
+          options={{ drawerIcon: () => <IconButton icon="dumbbell" /> }}
+        />
+        <Drawer.Screen
+          name="Plans"
+          component={PlanList}
+          options={{ drawerIcon: () => <IconButton icon="calendar-outline" /> }}
+        />
+        <Drawer.Screen
+          name="Graphs"
+          component={GraphsList}
+          options={{
+            drawerIcon: () => <IconButton icon="chart-bell-curve-cumulative" />,
+          }}
+        />
+        <Drawer.Screen
+          name="Timer"
+          component={TimerPage}
+          options={{ drawerIcon: () => <IconButton icon="timer-outline" /> }}
+        />
+        <Drawer.Screen
+          name="Weight"
+          component={WeightList}
+          options={{ drawerIcon: () => <IconButton icon="scale-bathroom" /> }}
+        />
+        <Drawer.Screen
+          name="Insights"
+          component={InsightsPage}
+          options={{
+            drawerIcon: () => <IconButton icon="lightbulb-on-outline" />,
+          }}
+        />
+        <Drawer.Screen
+          name="Settings"
+          component={SettingsPage}
+          options={{ drawerIcon: () => <IconButton icon="cog-outline" /> }}
+        />
+      </Drawer.Navigator>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  debugBanner: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    transform: [{ rotate: '45deg' }],
+    backgroundColor: 'red',
+    zIndex: 1000,
+  },
+  debugText: {
+    color: 'white',
+    padding: 5,
+    fontSize: 10,
+  },
+});
